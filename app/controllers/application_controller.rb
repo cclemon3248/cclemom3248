b307362fc9d0fdd7c60bf0a1d9812e725129b348
junho jang <cclemon3248@gmail.com>
2022-04-25 14:15:11 +0900

    必要のないテストファイルは作成しないように、config/application.rbを編集。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  config.generators do |g|
+    g.test_framework :rspec,
+                     model_specs: true,
+                     view_specs: false,
+                     helper_specs: false,
+                     routing_specs: false,
+                     controller_specs: false,
+                     request_specs: false
+  end
 end
