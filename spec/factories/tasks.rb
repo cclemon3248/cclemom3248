44cfd21314d91379390579d2fc9ed4e41da88563
junho jang <cclemon3248@gmail.com>
2022-04-26 17:06:04 +0900

    task.rb作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,10 @@
 FactoryBot.define do
   factory :task do
     # 下記の内容は実際に作成するカラム名に合わせて変更してください
-    title { 'test_title' }
+    title { "test_title" }
     content { 'test_content' }
+    deadline { "2020/01/01" }
+    priority { "未着手" }
+    status { "低" }
   end
 end
