37783384623e8268a240c6ba21e2c2bcae1958ed
junho jang <cclemon3248@gmail.com>
2022-04-27 08:48:48 +0900

    タスクを新規作成テスト

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,20 +3,25 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
+        task = FactoryBot.create(:task, title: 'task')
+        # タスク一覧ページに遷移
+        visit tasks_path
+        # ページ遷移直後でデバッグ
+        
+        # タスク一覧ページに遷移できているかを確認
+        current_path
+        # タスクがデータベースに作成されているかを確認
+        Task.count
+        # 表示するHTMLにタスク情報が入っているかを確認
+        page.html
+        expect(page).to have_content 'task'
+        
       end
     end
   end
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        # テストで使用するためのタスクを作成
-        task = FactoryBot.create(:task, title: 'task')
-        # タスク一覧ページに遷移
-        visit tasks_path
-        # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
-        # have_contentされているか（含まれているか）ということをexpectする（確認・期待する）
-        expect(page).to have_content "task"
-        # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
       end
     end
   end
