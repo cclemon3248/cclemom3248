734bd8b7993b836ffe0050fa98b81f14662d9701
junho jang <cclemon3248@gmail.com>
2022-04-27 13:32:00 +0900

    モデルテスト実装

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,19 +2,21 @@ require 'rails_helper'
 RSpec.describe 'タスクモデル機能', type: :model do
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空の場合' do
-      it 'バリデーションにひっかる' do
-        task = Task.new(title: '', content: '失敗テスト')
+      it 'バリデーションにひっかかる' do
+        task = Task.new(title: '', content: '失敗テスト', deadline: "2022-03-31", priority: "低", status: "未実装")
         expect(task).not_to be_valid
       end
-    end   
-      context 'タスクの詳細が空の場合' do
+    end
+    context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
-        # ここに内容を記載する
+        task = Task.new(title: 'うわん', content: '', deadline: "2022-03-31", priority: "低", status: "未実装")
+        expect(task).not_to be_valid
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        # ここに内容を記載する
+        task = Task.new(title: 'うわん', content: '失敗テスト', deadline: "2022-03-31", priority: "低", status: "未実装")
+        expect(task).to be_valid
       end
     end
   end
