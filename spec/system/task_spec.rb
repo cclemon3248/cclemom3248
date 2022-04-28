26c467001b44e617bd0e2efa3d69b9405b54bfc3
junho jang <cclemon3248@gmail.com>
2022-04-28 15:23:53 +0900

    新しいタスクが一番上に表示されるテスト作成

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,40 +1,25 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  describe '新規作成機能' do
-    context 'タスクを新規作成した場合' do
-      it '作成したタスクが表示される' do
-        visit new_task_path
-        fill_in 'Title', with: 'デジモン'
-        fill_in 'Content', with: 'スパゲート'
-        fill_in 'Deadline', with: '2022-04-03'
-        fill_in 'Priority', with: '低'
-        fill_in 'Status', with: '未着手'
-        click_on 'aaa'
-        expect(page).to have_content "デジモン"
-        expect(page).to have_content "スパゲート"
-        expect(page).to have_content "2022-04-03"
-        expect(page).to have_content "低"
-        expect(page).to have_content "未着手"
-      end
-    end
+  let!(:task1) { FactoryBot.create(:task, title: 'task1') }
+  let!(:task2) { FactoryBot.create(:task, title: 'task2') }
+  let!(:task3) { FactoryBot.create(:task, title: 'task3') }
+  before do
+    # 「一覧画面に遷移した場合」や「タスクが作成日時の降順に並んでいる場合」など、contextが実行されるタイミングで、before内のコードが実行される
+    visit tasks_path
   end
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, title: 'task')
-        visit tasks_path
-        expect(page).to have_content 'task'
+        expect(page).to have_content 'task1'
       end
     end
-  end
-  
-  describe '詳細表示機能' do
-     context '任意のタスク詳細画面に遷移した場合' do
-       it '該当タスクの内容が表示される' do
-        task = FactoryBot.create(:task, title: 'task')
-        visit task_path(task.id)
-        expect(page).to have_content 'task'
-       end
-     end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task_list = all('.task_row') 
+        expect(task_list[0]).to have_content 'task3'
+        expect(task_list[1]).to have_content 'task2'
+        expect(task_list[2]).to have_content 'task1'
+      end
+    end
   end
 end
