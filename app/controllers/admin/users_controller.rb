7868e112ab4138de2e9ad191e7a6e90bf2d4998d
junho jang <cclemon3248@gmail.com>
2022-05-15 02:44:25 +0900

    修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,9 +2,9 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.includes(:tasks)
     # @user = User.all
-    # unless current_user.try(:admin?)
-    #   redirect_to tasks_path, notice: "管理者以外は立ち入り禁止です！"
-    # end
+    unless current_user.try(:admin?)
+      redirect_to tasks_path, notice: "管理者以外は立ち入り禁止です！"
+    end
   end
 
   def edit
